59bed4be5ea9dec080acb4cd26b594053fd41ada
Solofo <solofo@precision.mg>
2017-08-18 18:35:36 +0300

    Web server

--- src/main/java/io/vertx/test/App.java
+++ src/main/java/io/vertx/test/App.java
@@ -1,11 +1,21 @@
 package io.vertx.test;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.StaticHandler;
 
 public class App extends AbstractVerticle {
 
     @Override
     public void start() {
-        System.out.println("Hello World!");
+        Router router = Router.router(vertx);
+
+        router.get("/").handler((RoutingContext req) -> req.response().end("Hello world!"));
+
+        vertx
+                .createHttpServer()
+                .requestHandler(router::accept)
+                .listen(4000);
     }
 }
