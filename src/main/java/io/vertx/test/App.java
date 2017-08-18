13e7de3a2068b4f08234608d54696abbf1a6d304
Solofo <solofo@precision.mg>
2017-08-18 18:24:16 +0300

    Initial commit

---
+++ src/main/java/io/vertx/test/App.java
@@ -0,0 +1,11 @@
+package io.vertx.test;
+
+import io.vertx.core.AbstractVerticle;
+
+public class App extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        System.out.println("Hello World!");
+    }
+}
