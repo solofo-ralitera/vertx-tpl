79b58817cacc03889537c324130cf39b840e5058
Solofo <solofo@precision.mg>
2017-08-30 19:23:18 +0300

    Whiteboard : init new connexion board

--- src/main/java/io/vertx/test/Socket.java
+++ src/main/java/io/vertx/test/Socket.java
@@ -2,7 +2,9 @@ package io.vertx.test;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.StaticHandler;
@@ -14,6 +16,9 @@ import io.vertx.ext.web.templ.ThymeleafTemplateEngine;
 
 
 public class Socket extends AbstractVerticle {
+    private String Msg_LastMessage = "";
+    private String Msg_CurrentLanguage = "";
+    private String Msg_TextSelection = "";
 
     @Override
     public void start(final Future<Void> startFuture) {
@@ -25,6 +30,11 @@ public class Socket extends AbstractVerticle {
         router.route("/templates/*").handler(StaticHandler.create("templates"));
 
         // EventBus
+        EventBus eventBus = vertx.eventBus();
+        eventBus.consumer("board-message").handler(message -> Msg_LastMessage = message.body().toString());
+        eventBus.consumer("board-language").handler(message -> Msg_CurrentLanguage = message.body().toString());
+        eventBus.consumer("board-textselection").handler(message -> Msg_TextSelection = message.body().toString());
+
         SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
         BridgeOptions options = new BridgeOptions()
                 .addInboundPermitted(new PermittedOptions().setAddress("board-message"))
@@ -33,16 +43,26 @@ public class Socket extends AbstractVerticle {
                 .addOutboundPermitted(new PermittedOptions().setAddress("board-language"))
                 .addInboundPermitted(new PermittedOptions().setAddress("board-textselection"))
                 .addOutboundPermitted(new PermittedOptions().setAddress("board-textselection"))
+                .addOutboundPermitted(new PermittedOptions().setAddress("board-newconnection"))
         ;
         sockJSHandler.bridge(options, event -> {
             if (event.type() == BridgeEventType.SOCKET_CREATED) {
                 //System.out.println("A socket was created");
+                event.socket().write(new JsonObject()
+                        .put("address", "board-newconnection")
+                        .put("body", "init board")
+                        .put("lastmessage", Msg_LastMessage)
+                        .put("language", Msg_CurrentLanguage)
+                        .put("textselection", Msg_TextSelection)
+                        .encode()
+                );
             }
             else if (event.type() == BridgeEventType.RECEIVE) {
                 //System.out.println(event.socket().writeHandlerID());
             }
             event.complete(true);
         });
+
         router.route("/eventbus/*").handler(sockJSHandler);
 
         // Index
